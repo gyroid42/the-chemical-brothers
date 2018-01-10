7c1278c30b1a3851e586c2d98612d8f701cbaccc
Jock MacDonald <1501651@abertay.ac.uk>
2018-01-10 18:51:30 +0000

    started implementation of game controller
    game control switches between app states and changes the camera
    
    camera still needs to calculate the rotation speed for the motion

---
+++ ChemistryBrothers/Assets/Scripts/GameController.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum APPSTATE
+{
+    PLAYING,
+    MAINMENU,
+    LEADERBOARD,
+    GAMEOVER
+}
+
+public class GameController : MonoBehaviour {
+
+    public static GameController controller_;
+
+
+    public Main_Camera_Behaviour camera_;
+    public APPSTATE currentAppState_;
+    public List<Vector3> stepLocations_;
+    public Vector3 mainMenuLocation_;
+    public Vector3 leaderBoardLocation_;
+
+
+    private int currentStep_;
+    public int totalSteps_;
+
+
+    void Awake()
+    {
+        if (controller_ == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            controller_ = this;
+        }
+        else if (controller_ != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+        //camera_ = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Main_Camera_Behaviour>();
+
+        currentAppState_ = APPSTATE.MAINMENU;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (currentAppState_ == APPSTATE.PLAYING)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                GotoNextStep();
+            }
+        }
+        else if (currentAppState_ == APPSTATE.MAINMENU)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartGame();
+            }
+        }
+        
+	}
+
+
+    public void StartGame()
+    {
+        ChangeAppState(APPSTATE.PLAYING);
+        currentStep_ = 0;
+        camera_.GotoPosition(stepLocations_[currentStep_]);
+    }
+
+    public void EndGame()
+    {
+        camera_.GotoPosition(mainMenuLocation_);
+        ChangeAppState(APPSTATE.MAINMENU);
+    }
+
+
+    void GotoNextStep()
+    {
+        if (currentAppState_ == APPSTATE.PLAYING)
+        {
+
+            currentStep_++;
+            if (currentStep_ >= totalSteps_)
+            {
+                EndGame();
+            }
+            else
+            {
+                camera_.GotoPosition(stepLocations_[currentStep_]);
+            }
+        }
+    }
+
+
+    void ChangeAppState(APPSTATE newState)
+    {
+        switch (currentAppState_)
+        {
+            case APPSTATE.LEADERBOARD:
+                break;
+            case APPSTATE.MAINMENU:
+                break;
+            case APPSTATE.PLAYING:
+                break;
+            case APPSTATE.GAMEOVER:
+                break;
+        }
+
+        currentAppState_ = newState;
+
+        switch (currentAppState_)
+        {
+            case APPSTATE.LEADERBOARD:
+                break;
+            case APPSTATE.MAINMENU:
+                break;
+            case APPSTATE.PLAYING:
+                break;
+            case APPSTATE.GAMEOVER:
+                break;
+        }
+
+    }
+
+
+    public int CurrentStep()
+    {
+        return currentStep_;
+    }
+
+}
