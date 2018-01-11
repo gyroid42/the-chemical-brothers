65a7456840bdd74d291d1b1abc85122d886e40b6
Jock MacDonald <1501651@abertay.ac.uk>
2018-01-11 14:28:02 +0000

    most recent thingy

--- ChemistryBrothers/Assets/Scripts/GameController.cs
+++ ChemistryBrothers/Assets/Scripts/GameController.cs
@@ -7,17 +7,30 @@ public enum APPSTATE
 {
     PLAYING,
     MAINMENU,
-    LEADERBOARD,
     GAMEOVER
+}
+
+public enum STEPSTATE
+{
+    WORKING,
+    STARTSCREEN,
+    TRANSITIONINGTOSTART,
+    TRANSITIONINGTOWORKING
 }
 
 public class GameController : MonoBehaviour {
 
     public static GameController controller_;
 
+    public List<BaseInteractable> interactables_;
+    private List<List<int>> stepInteractables_;
+    private List<BaseInteractable> activeInteractables_;
+
+    public int mistakes_;
 
     public Main_Camera_Behaviour camera_;
     public APPSTATE currentAppState_;
+    private STEPSTATE currentStepState_;
 
     public List<Vector3> stepRotations_;
     public List<Vector3> stepLocations_;
@@ -52,6 +65,23 @@ public class GameController : MonoBehaviour {
 
         //camera_ = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Main_Camera_Behaviour>();
 
+        camera_.transform.position = mainMenuLocation_;
+        camera_.transform.eulerAngles = mainMenuRotation_;
+
+        stepInteractables_ = new List<List<int>>();
+        stepInteractables_.Add(new List<int> { 0 });
+        stepInteractables_.Add(new List<int> { 1 });
+        stepInteractables_.Add(new List<int> { 0 });
+        stepInteractables_.Add(new List<int> { 1 });
+        stepInteractables_.Add(new List<int> { 0 });
+        stepInteractables_.Add(new List<int> { 1 });
+        stepInteractables_.Add(new List<int> { 0 });
+        stepInteractables_.Add(new List<int> { 1 });
+        stepInteractables_.Add(new List<int> { 0 });
+        stepInteractables_.Add(new List<int> { 1 });
+        stepInteractables_.Add(new List<int> { 0 });
+        activeInteractables_ = new List<BaseInteractable>();
+
         currentAppState_ = APPSTATE.MAINMENU;
 	}
 	
@@ -60,10 +90,34 @@ public class GameController : MonoBehaviour {
 		
         if (currentAppState_ == APPSTATE.PLAYING)
         {
-            if (Input.GetMouseButtonDown(0))
+
+            switch (currentStepState_)
             {
-                GotoNextStep();
+                case STEPSTATE.STARTSCREEN:
+                    StartStepUpdate();
+                    break;
+
+                case STEPSTATE.WORKING:
+                    StepUpdate();
+                    break;
+
+
+                case STEPSTATE.TRANSITIONINGTOSTART:
+                    if (camera_.FinishedMove())
+                    {
+                        currentStepState_ = STEPSTATE.STARTSCREEN;
+                    }
+                    break;
+
+                case STEPSTATE.TRANSITIONINGTOWORKING:
+                    if (camera_.FinishedMove())
+                    {
+                        currentStepState_ = STEPSTATE.WORKING;
+                    }
+                    break;
+
             }
+
         }
         else if (currentAppState_ == APPSTATE.MAINMENU)
         {
@@ -79,31 +133,15 @@ public class GameController : MonoBehaviour {
     public void StartGame()
     {
         currentStep_ = 0;
-        camera_.GotoPosition(stepLocations_[currentStep_], stepRotations_[currentStep_]);
+        currentStepState_ = STEPSTATE.STARTSCREEN;
+        camera_.GotoPosition(stepLocations_[0], stepRotations_[0]);
     }
 
     public void EndGame()
     {
         camera_.GotoPosition(mainMenuLocation_, mainMenuRotation_);
         ChangeAppState(APPSTATE.MAINMENU);
-    }
-
-
-    public void GotoNextStep()
-    {
-        if (currentAppState_ == APPSTATE.PLAYING)
-        {
-
-            currentStep_++;
-            if (currentStep_ >= totalSteps_)
-            {
-                EndGame();
-            }
-            else
-            {
-                camera_.GotoPosition(stepLocations_[currentStep_], stepRotations_[currentStep_]);
-            }
-        }
+        Debug.Log("end game has been called");
     }
 
 
@@ -111,8 +149,6 @@ public class GameController : MonoBehaviour {
     {
         switch (currentAppState_)
         {
-            case APPSTATE.LEADERBOARD:
-                break;
             case APPSTATE.MAINMENU:
                 break;
             case APPSTATE.PLAYING:
@@ -125,8 +161,6 @@ public class GameController : MonoBehaviour {
 
         switch (currentAppState_)
         {
-            case APPSTATE.LEADERBOARD:
-                break;
 
             case APPSTATE.MAINMENU:
                 break;
@@ -147,5 +181,63 @@ public class GameController : MonoBehaviour {
     {
         return currentStep_;
     }
+
+
+
+    private void StepUpdate()
+    {
+
+        for (int i = 0; i < activeInteractables_.Count; i++)
+        {
+            activeInteractables_[i].UpdateInteractable();
+        }
+
+        if ( Input.GetMouseButtonDown(0))
+        {
+            EndStep();
+        }
+    }
+
+    private void StartStepUpdate()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            activeInteractables_.Clear();
+            for (int i = 0; i < stepInteractables_[currentStep_].Count; i++) {
+
+                activeInteractables_.Add(interactables_[stepInteractables_[currentStep_][i]]);
+
+                interactables_[stepInteractables_[currentStep_][i]].EnterWorkArea();
+            }
+            camera_.GotoPosition(stepLocations_[1], stepRotations_[1]);
+            currentStepState_ = STEPSTATE.TRANSITIONINGTOWORKING;
+        }
+    }
+
+    private void EndStep()
+    {
+        if (currentAppState_ == APPSTATE.PLAYING)
+        {
+            currentStep_++;
+
+            for (int i = 0; i < activeInteractables_.Count; i++)
+            {
+                activeInteractables_[i].ExitWorkArea();
+            }
+
+            if (currentStep_ >= totalSteps_)
+            {
+                EndGame();
+            }
+            else
+            {
+                camera_.GotoPosition(stepLocations_[0], stepRotations_[0]);
+                currentStepState_ = STEPSTATE.TRANSITIONINGTOSTART;
+
+            }
+        }
+    }
+
 
 }
---
+++ ChemistryBrothers/Assets/Scripts/Interactables/BalanceScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceScript : BaseInteractable {
+
+
+
+
+    public override void EnterWorkArea()
+    {
+        base.EnterWorkArea();
+    }
+
+    public override void ExitWorkArea()
+    {
+        base.ExitWorkArea();
+    }
+
+
+    public override void UpdateInteractable()
+    {
+        base.UpdateInteractable();
+    }
+}
