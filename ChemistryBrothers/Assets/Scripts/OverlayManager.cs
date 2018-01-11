5a48d2a8313a0df7d25ce5e38408da93de2ffcdf
JakeMDuthie <jakeduthie@rocketmail.com>
2018-01-11 19:12:26 +0000

    Molecule Viewer Implemented to Main Scene
    - Integrated molecule viewer
    - Can be toggled with magnifying glass in game
    - Blackboard UI icons enlarge when hovered over

---
+++ ChemistryBrothers/Assets/Scripts/OverlayManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayManager : MonoBehaviour {
+    
+    private bool moleculeViewer_;
+
+	// Use this for initialization
+	void Start () {
+        moleculeViewer_ = false;
+        gameObject.SetActive(moleculeViewer_);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    // When called, alternates the state of the molecule viewer
+    public void ToggleMoleculeViewer()
+    {
+        moleculeViewer_ = !moleculeViewer_;
+        // Only render the molecule viewer when instructed
+        gameObject.SetActive(moleculeViewer_);
+        Debug.Log("Detected");
+    }
+
+    public bool MoleculeViewer()
+    {
+        return moleculeViewer_;
+    }
+}
