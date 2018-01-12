04c76655c675b64fb6e3e7b39f5d7d4ecc1a0feb
Jock MacDonald <1501651@abertay.ac.uk>
2018-01-12 12:09:23 +0000

    fixing dat sweet only 1 step prototype

---
+++ ChemistryBrothers/Assets/Scripts/BlackboardManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackboardManager : MonoBehaviour {
+
+    public GameObject MainDisplay;
+    public GameObject ScoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+        MainDisplay.SetActive(true);
+        ScoreDisplay.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if(GameController.controller_.currentAppState_ == )
+        //{
+        // THIS WILL CONTAIN FUNCTION FOR UI SWITCHING
+        //}
+	}
+
+    // Functions to change the screen displayed on the blackboard are below
+    public void SetDisplayMain()
+    {
+        MainDisplay.SetActive(true);
+        ScoreDisplay.SetActive(false);
+    }
+
+    public void SetDisplayScore()
+    {
+        MainDisplay.SetActive(false);
+        ScoreDisplay.SetActive(true);
+    }
+}
